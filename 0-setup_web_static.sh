d930d49f294ca578bbeacb829aa2aecbea6fa5c9
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-10 22:24:54 +0000

    Commit # 03: Upload 0-setup_web_static.sh script file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
 sudo service nginx stop
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get -y install nginx
+sudo apt-get -y update && sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 echo "Holberton School rules!" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
