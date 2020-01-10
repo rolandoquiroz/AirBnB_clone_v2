846668a91879ff81c7670b77245c3fab57d95666
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-10 22:15:13 +0000

    Commit # 03: Upload 0-setup_web_static.sh script file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,5 +7,5 @@ sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 echo "Holberton School rules!" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '/^\tlocation \/ {$/i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}\n' etc/nginx/sites-available/default
+sudo sed -i '/^\tlocation \/ {$/i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
