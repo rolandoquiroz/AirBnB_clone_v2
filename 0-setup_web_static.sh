c9fc80746825106048f6614920c4dc5d65f4cf96
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-11 03:06:27 +0000

    [Re-do] Tasks 0 Enhance syntax

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,13 @@
 sudo service nginx stop
 sudo apt-get -y update && sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
-echo "Holberton School rules!" | sudo tee /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School rules!
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '/^\tlocation \/ {$/i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
