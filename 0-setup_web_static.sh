615c6f4c1a71f6460d2a4ffcf4abfe791d3dd64f
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-11 03:44:46 +0000

    [Re-do] Tasks 0 Enhance syntax

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,5 +12,5 @@ echo "<html>
 </html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '/^\tlocation \/ {$/i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -ie '/^\tlocation \/ {$/i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
