af111b3f944a69829859cf07961d32101f0669a9
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-20 23:10:15 +0000

    Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,6 +6,9 @@ listening on 0.0.0.0, port 5000
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',strict_slashes=False)
 def hello_HBNB():
     return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0', port=5000)
