ab6ceead71c93a51b35243638e5b19a0a4169676
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-21 15:10:30 +0000

    [Upload] Adds Tasks 0 and 1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,6 +9,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_HBNB():
+    """
+    Display “Hello HBNB!”
+    """
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+listening on 0.0.0.0, port 5000
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """
+    Display “Hello HBNB!”
+    """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """
+    Display “HBNB”
+    """
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
