6f5c6b9153a5d2d1468e9cb13a491c2d61f58a7e
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-23 03:03:21 +0000

    web_flask/templates/7-states_list.html

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -21,7 +21,7 @@ def states_list():
     """
     List states in a jinja template
     """
-    return render_template('7-states_list.html', answer=storage.all("State"))
+    return render_template('7-states_list.html', states=storage.all("State"))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
